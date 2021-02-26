50f974237e42e4b6d4ad51b43944ffb4dcf19890
KrishnanSG <krishsg525@gmail.com>
2021-02-26 23:03:59 +0530

    hash: Refactor hash table functions

--- eves_game/resources/hash.py
+++ eves_game/resources/hash.py
@@ -1,38 +1,40 @@
 from collections import defaultdict
+from pprint import pprint
 from typing import List
 
 
 class HashTable:
-    def __init__(self, size) -> None:
-        self.data = defaultdict(List)
+    def __init__(self, size: int) -> None:
+        self.hash_table = defaultdict(list)
         self.max_size = size
 
     def generate_key(self, key: str) -> int:
-        temp = [i for i in key]
-        hash_key = sum(temp) % self.size
+        temp = [ord(i) for i in key]
+        hash_key = sum(temp) % self.max_size
         return hash_key
 
-    def add(self, key: str, value: str) -> None:
-        if key in self.data:
-            raise Exception("Value already alloted to the slot !")
+    def add(self, key: str):
+        if key in self.hash_table:
+            raise Exception("Value already allotted to the slot !")
 
-        self.data[self.generate_key(key)].append(value)
-
-        if self.max_size == self.size():
-            self.max_size *= 2
+        hash_value = self.generate_key(key)
+        self.hash_table[hash_value].append(key)
+        return hash_value
 
     def get(self, key: str) -> List:
-        if self.generate_key(key) not in self.data:
+        if self.generate_key(key) not in self.hash_table:
             return None
-        return self.data[key]
+        return self.hash_table[key]
 
     def delete(self, key: str) -> None:
-        if self.generate_key(key) not in self.data:
+        if self.generate_key(key) not in self.hash_table:
             raise Exception("Key not found !")
-        del self.data[self.generate_key(key)]
+        del self.hash_table[self.generate_key(key)]
 
     def size(self) -> int:
-        return len(self.data.keys())
-    
+        return len(self.hash_table.keys())
+
     def print_data(self):
-        print(self.data)
+        pprint(dict(self.hash_table))
+
+
