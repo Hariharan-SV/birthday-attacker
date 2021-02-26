5e245b7d49c6b021efbfbde2a9d42b9d87c8a856
Hariharan-SV <harikumar20112000@gmail.com>
2021-02-26 21:27:04 +0530

    Initial setup

---
+++ eves_game/simulator.py
@@ -0,0 +1,18 @@
+"""
+create a hash table of size k
+our own hash function
+create k+1 users
+for k+1 users easy collision
+
+check hashed password in a loop and get the user details
+use that original password and login to that user and exploit details
+
+use that probablity to observe collision
+
+hash table should be of dynamic size
+show that as collision occurs at some size
+
+create a table (defaultdict : user and passwd)
+add a new user into dict with passwd as key and user in a list
+hashfn = modn
+"""
