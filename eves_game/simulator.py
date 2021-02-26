2300bd04c243d652764b14424e4413063d26a457
KrishnanSG <krishsg525@gmail.com>
2021-02-26 23:05:06 +0530

    Initial simulator setup

--- eves_game/simulator.py
+++ eves_game/simulator.py
@@ -16,3 +16,20 @@ create a table (defaultdict : user and passwd)
 add a new user into dict with passwd as key and user in a list
 hashfn = modn
 """
+from pprint import pprint
+
+from eves_game.resources.hash import HashTable
+
+if __name__ == '__main__':
+    h = HashTable(5)
+    users = {
+        'hari': 0,
+        'krish': 0
+
+    }
+    v = h.add("hari")
+    users['hari'] = v
+    v = h.add("krishnan")
+    users['krish'] = v
+    pprint(users)
+    h.print_data()
