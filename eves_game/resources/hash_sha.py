64e13795591ed8888711e7df50bc7d86bc34b265
Hariharan-SV <harikumar20112000@gmail.com>
2021-02-27 07:51:57 +0530

    Add SHA hash table simulator

--- eves_game/resources/hash_sha.py
+++ eves_game/resources/hash_sha.py
@@ -1,6 +1,6 @@
 from collections import defaultdict
 from pprint import pprint
-
+import hashlib 
 
 class HashTable:
     def __init__(self, size: int) -> None:
@@ -8,32 +8,17 @@ class HashTable:
         self.max_size = size
 
     def generate_key(self, key: str, nounce=0) -> int:
-        temp = [ord(i) for i in key]
-        hash_key = (sum(temp) + nounce) % self.max_size
-        return hash_key
-
-    def double_table(self):
-        temp_table = defaultdict(list)
-        self.max_size = 2*self.max_size
-
-        for values in self.hash_table.values():
-            for value in values:
-                key = self.generate_key(value)
-                temp_table[key].append(value)
-        self.hash_table = temp_table.copy()
-        del temp_table
+        hash_key = hashlib.sha256(key.encode())
+        return hash_key.hexdigest()
 
     def add(self, key: str):
 
         hash_value = self.generate_key(key)
         collision = False
 
-        while self.hash_table.get(hash_value):
+        if self.hash_table.get(hash_value):
             print(f"Hash Collision detected for hash value {hash_value}!")
-            print("Doubling Table !")
-            self.double_table()
-            hash_value = self.generate_key(key)
-
+            
         self.hash_table[hash_value].append(key)
         return dict(hash_value=hash_value, collision_status=collision)
 
